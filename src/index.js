999e533f32b3889de1a329979cd66205d0f468c1
unknown <blissimeonx@gmail.com>
2026-03-26 16:18:20 +0100

    fixing pixel size

--- src/index.js
+++ src/index.js
@@ -10,13 +10,16 @@ import job from './lib/cron.js'
 const app = express()
 const PORT = process.env.PORT || 3000
 job.start()
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+// app.use(express.json());
 app.use(cors());
 
 
 
 app.use("/api/auth", authRoutes)
 app.use("/api/books", bookRoutes)
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB()

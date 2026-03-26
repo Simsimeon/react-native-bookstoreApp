5f401b2a8c9408f397afa1d084d3889fc1362f7c
unknown <blissimeonx@gmail.com>
2026-03-26 19:28:03 +0100

    fixing issues in auth routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -54,7 +54,8 @@ router.post("/register", async (req, res) => {
                 id: user._id,
                 username: user.username,
                 email: user.email,
-                profileImage: user.profileImage
+                profileImage: user.profileImage,
+                createdAt: user.createdAt,
             }
         })
     } catch (err) {
@@ -81,7 +82,8 @@ router.post("/login", async (req, res) => {
                 id: user._id,
                 username: user.username,
                 email: user.email,
-                profileImage: user.profileImage
+                profileImage: user.profileImage,
+                createdAt: user.createdAt,
             },
         })
     } catch (err) {

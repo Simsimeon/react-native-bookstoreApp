f8d42d5e7ad3108264181b671156f75db38324d0
unknown <blissimeonx@gmail.com>
2026-03-27 09:51:29 +0100

    fixing the delete end point in the backend

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -75,7 +75,7 @@ router.get("/user", protectedRoutes, async (req, res) => {
 
 router.delete("/:id", protectedRoutes, async (req, res) => {
     try {
-        const book = await Book.findById(res.params.id)
+        const book = await Book.findById(req.params.id)
         if (!book) return res.status(404).json({ message: "Book not found" })
         // check if the user is the creator of the book
         if (book.user.toString() !== req.user._id) {
@@ -84,7 +84,7 @@ router.delete("/:id", protectedRoutes, async (req, res) => {
             })
         }
         // delete image from cloudinary as well
-        if (book.image & book.image.includes("cloudinary")) {
+        if (book.image && book.image.includes("cloudinary")) {
             try {
                 const publicId = book.image.split("/").pop().split(".")[0]
                 await cloudinary.uploader.destroy(publicId)

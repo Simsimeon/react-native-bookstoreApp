79b007d4827f97122b90955fc3e8386dadce839b
Simsimeon <117627355+Simsimeon@users.noreply.github.com>
2026-03-29 14:57:05 +0100

    Clean up db.js by removing commented connection string
    Removed commented-out MongoDB connection string.

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,7 +4,6 @@ export const connectDB=async ()=>{
     try{
    const conn=   await mongoose.connect(
      process.env.MONGO_URI
-    // "mongodb://blissimeonx_db_user:w8Cb9juy7S3BUqyI@ac-uiblibz-shard-00-00.4xvkppo.mongodb.net:27017,ac-uiblibz-shard-00-01.4xvkppo.mongodb.net:27017,ac-uiblibz-shard-00-02.4xvkppo.mongodb.net:27017/?ssl=true&replicaSet=atlas-zv5pgu-shard-0&authSource=admin&appName=Cluster0"
 )
    console.log(`Database connected ${conn.connection.host}`);
    
